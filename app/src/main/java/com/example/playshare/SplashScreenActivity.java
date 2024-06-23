e8b83f43b0adc7fb7685897b9b0819807d5b2e6e
ברינה איתמר <126776819+itamarbrina@users.noreply.github.com>
2024-06-23 22:11:41 +0300

    adding splash screen to the application

---
+++ app/src/main/java/com/example/playshare/SplashScreenActivity.java
@@ -0,0 +1,23 @@
+package com.example.playshare;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3500);
+    }
+
+}
