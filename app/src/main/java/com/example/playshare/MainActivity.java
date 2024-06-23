1e8306fddac17c39fc17057d24c789ea69393fdf
ברינה איתמר <126776819+itamarbrina@users.noreply.github.com>
2024-06-23 23:37:40 +0300

    adding button listener to topappbar

--- app/src/main/java/com/example/playshare/MainActivity.java
+++ app/src/main/java/com/example/playshare/MainActivity.java
@@ -4,20 +4,26 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private TextView nameEditText, heightEditText, ageEditText;
     private Button nextButton;
+    private MaterialToolbar topAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +34,18 @@ public class MainActivity extends AppCompatActivity {
         heightEditText = findViewById(R.id.heightEditText);
         ageEditText = findViewById(R.id.ageEditText);
         nextButton = findViewById(R.id.nextButton);
+        topAppBar = findViewById(R.id.topAppBar);
 
         requestLocationPermission();
+
+        topAppBar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.profile) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            }
+            return false;
+        });
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
