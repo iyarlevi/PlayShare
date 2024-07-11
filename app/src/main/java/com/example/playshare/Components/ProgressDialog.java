355407a29aa635d0ff70ce89c34377b4dd9cb30d
ברינה איתמר <126776819+itamarbrina@users.noreply.github.com>
2024-07-11 10:02:03 +0300

    make the dialog appear on all the screen

--- app/src/main/java/com/example/playshare/Components/ProgressDialog.java
+++ app/src/main/java/com/example/playshare/Components/ProgressDialog.java
@@ -2,24 +2,25 @@ package com.example.playshare.Components;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.view.Window;
-import android.widget.ProgressBar;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.example.playshare.R;
 
 public class ProgressDialog extends Dialog {
-    private ProgressBar progressBar;
     private final TextView progressText;
 
     public ProgressDialog(Context context) {
         super(context);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.progress_dialog);
+        if (getWindow() != null) {
+            getWindow().setBackgroundDrawable(new ColorDrawable(context.getColor(R.color.colorPrimary)));
+            getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+        }
 
-        progressBar = findViewById(R.id.progressBar);
         progressText = findViewById(R.id.progressText);
 
         setCancelable(false);
