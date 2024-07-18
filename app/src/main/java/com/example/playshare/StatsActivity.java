7ac83d77b14d8fd35e6560cc6df12c557fb42b18
ברינה איתמר <126776819+itamarbrina@users.noreply.github.com>
2024-07-18 17:54:11 +0300

    Make the stats activity read from firebase

--- app/src/main/java/com/example/playshare/StatsActivity.java
+++ app/src/main/java/com/example/playshare/StatsActivity.java
@@ -5,23 +5,29 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.playshare.Components.BottomNavigator;
+import com.example.playshare.Components.ProgressDialog;
 import com.example.playshare.Components.TopAppBarMenuListener;
 import com.example.playshare.Connectors.FireStoreConnector;
 import com.example.playshare.Data.Enums.CollectionsEnum;
+import com.example.playshare.Data.Enums.GameTypeEnum;
+import com.example.playshare.Data.Models.GameModel;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Map;
 
 public class StatsActivity extends BaseActivityClass {
 
     private static final String TAG = "StatsActivity";
-    private TextView livePlayersTextView;
-    private TextView basketballPlayersTextView;
-    private TextView soccerPlayersTextView;
+    private TextView livePlayersTextView, basketballGamesTextView, soccerGamesTextView, tennisGamesTextView;
+    private ProgressDialog progressDialog;
+    private final FireStoreConnector firestore = FireStoreConnector.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +35,9 @@ public class StatsActivity extends BaseActivityClass {
         setContentView(R.layout.activity_stats);
 
         livePlayersTextView = findViewById(R.id.livePlayersTextView);
-        basketballPlayersTextView = findViewById(R.id.basketballPlayersTextView);
-        soccerPlayersTextView = findViewById(R.id.soccerPlayersTextView);
+        basketballGamesTextView = findViewById(R.id.basketballGamesTextView);
+        soccerGamesTextView = findViewById(R.id.soccerGamesTextView);
+        tennisGamesTextView = findViewById(R.id.tennisGamesTextView);
         Button mapButton = findViewById(R.id.mapButton);
         mapButton.setOnClickListener(v -> startActivity(new Intent(StatsActivity.this, MapActivity.class)));
 
@@ -46,21 +53,70 @@ public class StatsActivity extends BaseActivityClass {
         bottomNavigationView.setOnItemSelectedListener(bottomNavigator);
 
         // Set initial loading text
-        livePlayersTextView.setText(getString(R.string.loading));
-        basketballPlayersTextView.setText(getString(R.string.loading));
-        soccerPlayersTextView.setText(getString(R.string.loading));
-        FireStoreConnector firestore = FireStoreConnector.getInstance();
+        progressDialog = new ProgressDialog(this);
+        // todo add to strings.xml
+        progressDialog.setMessage("Loading stats...");
+        progressDialog.show();
+        getData();
+    }
 
-        // todo : get this directly from users data
-        firestore.getDocuments(CollectionsEnum.STATS.getCollectionName(),
+    private void getData() {
+        firestore.getDocuments(CollectionsEnum.USERS.getCollectionName(),
                 documents -> {
-                    Map<String, Object> document = documents.get(0);
-                    livePlayersTextView.setText("Live players: " + document.get("livePlayers"));
-                    basketballPlayersTextView.setText("Basketball Players: " + document.get("basketballPlayers"));
-                    soccerPlayersTextView.setText("Soccer Players: " + document.get("soccerPlayers"));
+                    int livePlayers = 0;
+                    SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.ENGLISH);
+                    Date currentDate = new Date();
+                    for (Map<String, Object> document : documents) {
+                        Object timeStamp = document.get("timeStamp");
+                        if (timeStamp != null) {
+                            String timeStampString = timeStamp.toString();
+                            try {
+                                Date playerDate = sdf.parse(timeStampString);
+                                if (playerDate == null) {
+                                    throw new ParseException("Date is null", 0);
+                                }
+                                long diff = currentDate.getTime() - playerDate.getTime();
+                                long diffDays = diff / (24 * 60 * 60 * 1000);
+                                if (diffDays <= 7) {
+                                    livePlayers++;
+                                }
+                            } catch (ParseException e) {
+                                Log.e(TAG, "Error parsing date: " + e.getMessage());
+                            }
+                        }
+                    }
+                    get_GamesStats();
+
+
+                    livePlayersTextView.setText(getString(R.string.live_players, String.valueOf(livePlayers)));
+                    progressDialog.dismiss();
+
                 }, e -> {
                     Log.e(TAG, "Error getting stats: " + e.getMessage());
-                    Toast.makeText(this, "Error getting stats: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    get_GamesStats();
                 });
     }
+
+    private void get_GamesStats() {
+        firestore.getDocuments(CollectionsEnum.GAMES.getCollectionName(),
+                documents -> {
+                    int basketballGames = 0;
+                    int soccerGames = 0;
+                    int tennisGames = 0;
+                    for (Map<String, Object> document : documents) {
+                        GameModel game = new GameModel(document);
+                        if (game.getType() == GameTypeEnum.BASKETBALL) {
+                            basketballGames++;
+                        } else if (game.getType() == GameTypeEnum.SOCCER) {
+                            soccerGames++;
+                        } else if (game.getType() == GameTypeEnum.TENNIS) {
+                            tennisGames++;
+                        }
+                    }
+                    basketballGamesTextView.setText(getString(R.string.basketball_games, String.valueOf(basketballGames)));
+                    soccerGamesTextView.setText(getString(R.string.soccer_games, String.valueOf(soccerGames)));
+                    tennisGamesTextView.setText(getString(R.string.tennis_games, String.valueOf(tennisGames)));
+                }, e -> Log.e(TAG, "Error getting games stats: " + e.getMessage()));
+    }
 }
+
